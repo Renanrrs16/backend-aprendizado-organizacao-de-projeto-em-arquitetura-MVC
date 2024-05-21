1f09e7a52bc30479839d4f6add86d6940bce48a5
Renan Souza <133045161+Renanrrs16@users.noreply.github.com>
2024-05-21 02:10:39 +0000

    limpeza e coemntarios no `index.js`

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
 require('dotenv').config()
 const express = require('express')
 const { connectToDatabase } = require('./db/database-connection')
+
+//Routers
 const personagemRouter = require('./personagem/personagem.router')
-//const { MongoClient, ObjectId } = require('mongodb')
-
-
 
 // Declaramos a funcao main()
 async function main() {
-// FIX: Utilizar o connectToDatabase() e receber o DB
-    await connectToDatabase()
+  //conectamos no DB
+  await connectToDatabase()
 
-    //const collection = db.collection('personagem')
+  //Inicamos o Express
+  const app = express()
 
-    const app = express()
+  //Middlewares
+  //Sinaliza para o Express que estamos usando JSON no Body
+  app.use(express.json())
 
-    //Middlewares
-    //Sinaliza para o Express que estamos usando JSON no Body
-    app.use(express.json())
+  //Endpoit de Hellor world
+  app.get('/', function (req, res) {
+    res.send('Hello World!')
+  })
 
-    app.get('/', function (req, res) {
-        res.send('Hello World!')
-    })
+  //Router
+  app.use('/personagem', personagemRouter)
 
-    app.use('/personagem', personagemRouter)
-
-    app.listen(3000, function () {
-        console.log('Aplicação rodando em http://localhost:3000')
-    })
+  app.listen(3000, function () {
+    console.log('Aplicação rodando em http://localhost:3000')
+  })
 }
 
 // Executamos a funcao main()
