c776aabd40b9f45f9446694bc09504db3f8eab0d
Renan Souza <133045161+Renanrrs16@users.noreply.github.com>
2024-05-21 00:40:57 +0000

    Criação inicial do service

---
+++ src/personagem/personagem.service.js
@@ -0,0 +1,23 @@
+function readAll() {
+  return []
+}
+
+function readById() {
+}
+
+function create() {
+}
+
+function updateById() {
+}
+
+function deleteByID() {
+}
+
+module.exports = {
+  readAll,
+  readById,
+  create,
+  updateById,
+  deleteByID
+}
